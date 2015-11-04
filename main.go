1ffafa6c129821ae3f9ca3365c7b7dcf49793c70
Manuel Valle <FcoManueel@gmail.com>
2015-11-04 02:37:13 -0700

    Added basic structures and interfaces

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+type Board [][]int8
+
+type Player struct{}
+type Players []Player
+
+type Move struct {
+    X int
+    Y int
+}
+type Moves []Move
+
+type Game struct {
+    board   [][]int8
+    players Players
+    history Moves
+}
+
+type Playable interface {
+    Init()
+    Board()
+    GetValidMoves() Moves
+    IsFinished() bool
+    NextPlayer() (playerIndex int)
+    ApplyMove(playerIndex int, move Move) Board
+}
+
+type PlayerInterface interface {
+    SelectMove(Game) Move
+}
+
+func (g *Game) Init() {}
+func (g *Game) Board() {}
+func (g *Game) GetValidMoves() Moves {
+    return Moves{}
+}
+func (g *Game) IsFinished() bool {
+    return false
+}
+
+func (g *Game) NextPlayer() (playerIndex int) {
+    return 0
+}
+
+func (g *Game) ApplyMove(playerIndex int, move Move) Board {
+    return Board{}
+}
+
+func main() {
+
+}
---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+import "testing"
+
+func TestGameInit(t *testing.T){}
+func TestGameBoard(t *testing.T){}
+func TestGameGetValidMoves(t *testing.T){}
+func TestGameIsFinished(t *testing.T){}
+func TestGameNextPlayer(t *testing.T){}
+func TestGameApplyMove(t *testing.T){}
